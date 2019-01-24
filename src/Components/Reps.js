2dafab214a4ce603454ed5e71ec7e920dda411bc
Michael Corey <fishincorey@gmail.com>
2019-01-23 21:51:06 -0700

    selectState list and api calls wroking

--- src/Components/Reps.js
+++ src/Components/Reps.js
@@ -3,12 +3,15 @@ import React, { Component, Fragment } from 'react'
 // import RepsData from '../testdata/repsdata'
 // import SensData from '../testdata/sensdata'
 
-import { repState } from '../filters'
+import { repState,  } from '../filters'
+import stateOptions from '../testdata/states_hash'
+
 
 import Rep from './Rep'
 
 export default class extends Component {
     state = {
+        legislativeBranch: '',
         selectState: '',
         repsmapped: [],
 
@@ -21,25 +24,45 @@ export default class extends Component {
 
     submitHandler = async (event) => {
         event.preventDefault()
-        const UtahReps = await fetch('http://localhost:3000/representatives/ut')
+        const baseUrl = 'http://localhost:3000/';
+        const postUrl = (baseUrl, legislativeBranch, selectState) => {
+            let newUrl = baseUrl + legislativeBranch + '/' + selectState ;
+            return newUrl;
+        }
+        const apiUrl = postUrl(baseUrl, this.state.legislativeBranch, this.state.selectState);
+
+        // const UtahReps = await fetch('http://localhost:3000/representatives/ut')
+        //     .then(result => result.json())
+        const StateReps = await fetch(apiUrl)
             .then(result => result.json())
-        console.log(UtahReps)
+        console.log(StateReps)
+        console.log(this.state.selectState , this.state.legislativeBranch)
         // this.setState({ repsmapped: this.renderReps(this.state.selectState) })
+        this.setState({ repsmapped: this.renderReps(this.state.selectState) })
+
     }
 
     render() {
-        // const repsmapped = this.renderReps()
-        const option = (<option value='TX'>Texas</option>)
+        const stateOption = Object.keys(stateOptions).map(
+            key => (<option value={key}>{Object.values(stateOptions[key])}</option>))
+
         return (
             <Fragment>
                 <form onSubmit={this.submitHandler}>
+                Legislative Branch: {" "}
+                    <select onChange={event => this.setState({ legislativeBranch: event.target.value })}>
+                        <option>Select Branch</option>
+                        <option value="representatives">House</option>
+                        <option value="senators">Senate</option>
+                    </select>
+                    
+                <br />
+
                     Select State: {" "}
                     <select onChange={event => this.setState({ selectState: event.target.value })}>
-                        <option>Select State</option>
-                        <option value="CA">CA</option>
-                        <option vlaue="UT">UT</option>
-                        {option}
+                        {stateOption}
                     </select>
+                    <br />
                     <input type="submit" value="Submit"></input>
                 </form>
                 <br />
--- src/filters/index.js
+++ src/filters/index.js
@@ -1,8 +1,11 @@
-// import RepsData from '../testdata/repsdata'
+import RepsData from '../testdata/repsdata'
+
 // // import SensData from '../testdata/sensdata'
 // import {findRepresentativesByState} from '../../server/find-rep-api'
 
-// export const repState = (selectState) => RepsData.filter(person => person.state === selectState)
+
+export const repState = (selectState) => RepsData.filter(person => person.state === selectState)
 // // export const repState2 = (selectState) => findRepresentativesByState.filter(person => person.state === selectState)
 
 // // export const repState = () => RepsData.filter(person => person.state = "UT")
+
